22b7a4a39032d65bcf39d422ec59dc88a3420b91
christhomp13 <theinfinitehumanblueprint@gmail.com>
2022-03-10 21:39:58 -0500

    Bring pages directory from Chris work

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
-import { AppProps } from 'next/app';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-import '../styles/global.css';
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
-
-export default MyApp;
+export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,32 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-import { AppConfig } from '../utils/AppConfig';
-
-// Need to create a custom _document because i18n support is not compatible with `next export`.
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang={AppConfig.locale}>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+  } from 'next/document'
+  
+  class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+      const initialProps = await Document.getInitialProps(ctx)
+  
+      return initialProps
+    }
+  
+    render() {
+      return (
+        <Html>
+          <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Exo&display=swap" rel="stylesheet"></link>
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      )
+    }
   }
-}
-
-export default MyDocument;
+  
+  export default MyDocument
